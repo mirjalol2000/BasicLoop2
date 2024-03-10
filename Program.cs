32fd81163ae988dd76b889ca75f7f9c343322c2b
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-10 12:51:46 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter numbers of array : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(99);
+}
+
+int max = 0, min = 100;
+
+for (int i = 0;i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine("Max = "+max+"    "+"min = "+min);
